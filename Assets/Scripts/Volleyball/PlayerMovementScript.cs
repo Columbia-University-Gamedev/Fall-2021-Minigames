1d7880c4696e3acd5a4a4dc6544053a9550dd454
KillyBOT <edwards.kyle.j@gmail.com>
2021-11-03 18:51:12 -0400

    Continued work on player input
    I finally understand how the input works. Sadly, I was listening to a zoom recitation and just realized I badly messed up my physics homework so I have to go do that real quick...

--- Assets/Scripts/Volleyball/PlayerMovementScript.cs
+++ Assets/Scripts/Volleyball/PlayerMovementScript.cs
@@ -1,40 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
     private Rigidbody2D rb2d;
+    private Transform tf;
     private bool isGrounded;
+    
+    private Vector2 movement;
+    private Vector2 playerVelocity;
 
-    public float playerSpeed = 2.0f;
-    public float jumpPower = 500.0f;
+    public bool isPlayerOne = true;
+    
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        tf = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tf.Translate(movement*Time.deltaTime);
     }
 
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.A)){
-            rb2d.velocity = new Vector2(-playerSpeed,rb2d.velocity.y);
-        }
-        if(Input.GetKeyDown(KeyCode.S)){
-            rb2d.velocity = new Vector2(playerSpeed,rb2d.velocity.y);
-        }
+    }
 
-        if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S)){
-            rb2d.velocity = new Vector2(0,rb2d.velocity.y);
+    void OnP1Move(InputValue m){
+        if(isPlayerOne){
+            movement = m.Get<Vector2>();
+            playerVelocity.x = movement.x * VolleyballConstants.playerSpeed * VolleyballConstants.playerVelocityMultiplier;
         }
-
     }
 
 }
---
+++ Assets/Scripts/Volleyball/VolleyballConstants.cs
@@ -0,0 +1,8 @@
+public static class VolleyballConstants
+{
+    public const float playerSpeed = 2.0f;
+    public const float playerJumpForce = 500.0f;
+    public const float playerChargeTime = 2.0f;
+    public const float playerVelocityMultiplier = 1.0f;
+    
+}
