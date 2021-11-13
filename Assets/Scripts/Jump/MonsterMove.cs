1f3d02b43675f08636c883c7e595c8cd7b5d18e9
el3031 <67562724+el3031@users.noreply.github.com>
2021-11-13 13:43:45 -0500

    wrote a monster movement script, idk if it works

---
+++ Assets/Scripts/Jump/MonsterMove.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class MonsterMove : MonoBehaviour
+{
+    public Vector3 minPos;
+    public Vector3 maxPos;
+    private Vector3 moveTowards = minPos;
+
+    public bool moveHorizontally;
+    
+    void Update()
+    {
+       if (moveHorizontally)
+       {
+           Horizontal();
+       }
+       else
+       {
+           Vertical();
+       }
+    }
+
+    void Horizontal()
+    {
+        if (transform.position.x == minPos.x)
+        {
+            moveTowards = maxPos;
+        }
+        else if (transform.position.x == maxPos.x)
+        {
+            moveTowards = minPos;
+        }
+        transform.position = Vector3.Lerp(transform.position, moveTowards, 0.5f);
+    }
+
+    void Vertical()
+    {
+        if (transform.position.y == minPos.y)
+        {
+            moveTowards = maxPos;
+        }
+        else if (transform.position.y == maxPos.y)
+        {
+            moveTowards = minPos;
+        }
+        transform.position = Vector3.Lerp(transform.position, moveTowards, 0.5f);
+    }
+}
