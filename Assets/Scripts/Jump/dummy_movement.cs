ab6cd2f674f05711ca8a79bb6b760569e5c52ec2
Alexander Rupp-Coppi <amr2311@columbia.edu>
2021-11-20 12:23:42 -0500

    jump script tweaks

--- Assets/Scripts/Jump/dummy_movement.cs
+++ Assets/Scripts/Jump/dummy_movement.cs
@@ -11,7 +11,16 @@ public class dummy_movement : MonoBehaviour
     public LayerMask ground;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f; 
-    public float jumpForce = 2f;   
+    public float jumpForce = 2f;
+
+    [SerializeField]
+    float _jumpHeight = 5f; // in meters
+
+    [SerializeField]
+    float _jumpTime = 1f; // in seconds
+
+    [SerializeField]
+    float _floorcastFudgeFactor = 0.23f; // magic number found through playtesting 
 
     // Start is called before the first frame update
     void Start()
@@ -22,23 +31,48 @@ public class dummy_movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (detectGround()){
-            Jump();
-            BetterJump();
+            // Jump();
+            // BetterJump();
+            rb.AddForce(Vector2.up * CalculateJumpForce());
         }
         //float currentPositionY = transform.position.y;
         //transform.position = new Vector3(transform.position.x, currentPositionY+0.1f, transform.position.z);
+    }
+
+    float CalculateJumpForce()
+    {
+        /*
+            F = (mass (targetVelocity - current_velocity)) / Time.deltaTime
+
+         */
+
+        // doesnt' work perfectly but if you play with the jump inputs 
+        // you can get good results
+
+        float h = _jumpHeight;
+        float t_flight = _jumpTime;
+
+        float vf = h / t_flight + 0.5f * Physics.gravity.magnitude * t_flight;
+
+        float m = rb.mass;
+        float v0 = rb.velocity.y;
+        float t_impulse = Time.deltaTime; 
+
+        return m * (vf - v0) / t_impulse; 
     }
    
 
     bool detectGround()
     {
-		Vector3 playerPosLeft = playerCollider.bounds.center - playerCollider.bounds.extents;
+        float size = playerCollider.bounds.extents.magnitude; 
+
+        Vector3 playerPosLeft = playerCollider.bounds.center - playerCollider.bounds.extents;
 		Vector3 playerPosRight = new Vector3(playerPosLeft.x + 2f * playerCollider.bounds.extents.x, playerPosLeft.y, playerPosLeft.z);
-		RaycastHit2D left = Physics2D.Raycast(playerPosLeft, Vector2.down, 0.5f, ground);
-		RaycastHit2D right = Physics2D.Raycast(playerPosRight, Vector2.down, 0.5f, ground);
+		RaycastHit2D left = Physics2D.Raycast(playerPosLeft, Vector2.down, _floorcastFudgeFactor * size, ground);
+		RaycastHit2D right = Physics2D.Raycast(playerPosRight, Vector2.down, _floorcastFudgeFactor * size, ground);
 
 
 		Color rayColor;
@@ -51,8 +85,8 @@ public class dummy_movement : MonoBehaviour
 		   rayColor = Color.red;
 		}	
 
-		Debug.DrawRay(playerPosLeft, Vector2.down * (playerCollider.bounds.extents.y + 0.5f), rayColor);
-		Debug.DrawRay(playerPosRight, Vector2.down * (playerCollider.bounds.extents.y + 0.5f), rayColor);
+		Debug.DrawRay(playerPosLeft, Vector2.down * _floorcastFudgeFactor * size, rayColor);
+		Debug.DrawRay(playerPosRight, Vector2.down * _floorcastFudgeFactor * size, rayColor);
 		
 		return (left.collider != null || right.collider != null);
     }
