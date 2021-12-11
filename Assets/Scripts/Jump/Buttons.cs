4ce11985290766c07e9207a9cd9174fa133474a6
el3031 <67562724+el3031@users.noreply.github.com>
2021-12-11 10:48:43 -0500

    buttons

--- Assets/Scripts/Jump/Buttons.cs
+++ Assets/Scripts/Jump/Buttons.cs
@@ -5,27 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    public OnStart()
+    [SerializeField] private Image pausePanel;
+    private float pausePanelOpacity;
+
+    [SerializeField] private GameObject UnpauseButton;
+    void Start()
+    {
+        pausePanelOpacity = pausePanel.color.a;
+    }
+    
+    public void OnStart()
     {
         SceneManager.LoadScene("GameScene");
     }
 
-    public OnPause()
+    public void OnPause()
     {
         Time.timeScale = 0f;
+        StartCoroutine(ImageFade.FadeImage(true, 0.5f, pausePanelOpacity, pausePanel));
+        StartCoroutine(ImageFade.FadeImage(false, 0.5f, 1f, pauseButton.GetComponent<Image>()));
+        pauseButton.GetComponent<Button>().enabled = true;
     }
 
-    public OnUnpause()
+    public void OnUnpause()
     {
+        StartCoroutine(Unpause());
+    }
+
+    public IEnumerator Unpause()
+    {
+        yield return new StartCoroutine(ImageFade.FadeImage(true, 0.5f, pausePanelOpacity, pausePanel));
         Time.timeScale = 1f;
     }
 
-    public OnQuit()
+    public void OnQuit()
     {
         #if (UNITY_EDITOR)
             UnityEditor.EditorApplication.isPlaying = false;            
         #else
             Application.Quit(0);
         #endif
+    }
+
+    public void OnHome()
+    {
+        SceneManager.LoadScene("HomeScreen");
     }
 }
