33f3e5c32cd55002ddf0bb0f09083befba694d65
Malik Endsley (R15 Laptop) <malik.s.endsley@gmail.com>
2022-02-27 14:20:29 -0500

    Testing materials for NPC setup

---
+++ Assets/ExploreCity/Scripts/EPCameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EPCameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = player.position + offset;
+    }
+}
