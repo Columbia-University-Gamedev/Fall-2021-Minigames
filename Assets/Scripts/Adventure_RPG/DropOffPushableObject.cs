2e8c489765fe2941841a0724003dfafd69b6a5a3
Pedro P <pedroaperezdr@gmail.com>
2022-03-27 12:28:42 -0400

    Scene 3

---
+++ Assets/Scripts/Adventure_RPG/DropOffPushableObject.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOffPushableObject : MonoBehaviour
+{
+    private Task task;
+    void Start()
+    {
+        task = GetComponent<Task>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PushableObject"))
+        {
+            //Makes it background item
+            Rigidbody2D otherRb = other.gameObject.GetComponent<Rigidbody2D>();
+            otherRb.constraints = RigidbodyConstraints2D.FreezeAll;
+            task.completed = true;
+        }
+    }
+
+}
