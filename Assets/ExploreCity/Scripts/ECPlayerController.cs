110175c4a4fc2c7bfe151780dd8202a910d051c0
Malik Endsley <malik.s.endsley@gmail.com>
2022-04-30 20:22:38 -0400

    Animated Player and set temp sprites for npc

--- Assets/ExploreCity/Scripts/ECPlayerController.cs
+++ Assets/ExploreCity/Scripts/ECPlayerController.cs
@@ -9,7 +9,8 @@ public class ECPlayerController : MonoBehaviour
     [SerializeField] 
     private Rigidbody2D rb2d;
     private Canvas canvas;
-
+    [SerializeField]
+    private Animator animator;
     private void Awake()
     {
 
@@ -29,6 +30,9 @@ public class ECPlayerController : MonoBehaviour
     {
         move = res.Get<Vector2>();
         Debug.Log("INPUT: Input Vector: " + move);
+        animator.SetFloat("horizontal", move.x);
+        animator.SetFloat("vertical", move.y);
+        animator.SetFloat("speed", move.sqrMagnitude);
     }
 
     public void OnInteract(InputValue value)
